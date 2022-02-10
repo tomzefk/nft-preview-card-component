b0135496eb32095bf319f38eb0eac1bab6744035
tomzefk <contacto@tomzefk.dev>
2022-02-10 11:46:49 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import nftImage from './images/nfts/image-equilibrium.jpg';
+import viewIcon from './images/icons/icon-view.svg';
+import ethIcon from './images/icons/icon-ethereum.svg';
+import clockIcon from './images/icons/icon-clock.svg';
+import creatorImage from './images/users/image-avatar.png';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nftWrapper'>
+        <div className='imageWrapper'>
+          <img className='nftImage' src={nftImage}></img>
+          <img className='viewIcon' src={viewIcon}></img>
+        </div>
+        <div className='info'>
+          <h1 className='title'>Equilibrium #3429</h1>
+          <p className='text'>Our Equilibrium collection promotes balance and calm.</p>
+          <div className='nftInfo'>
+            <div className='meta'>
+              <div className='price'>
+                <img src={ethIcon}></img>
+                <h4>0.041 ETH</h4>
+              </div>
+              <div className='timeleft'>
+                <img src={clockIcon}></img>
+                <h4>3 days left</h4>
+              </div>
+            </div>
+            <div className='creator'>
+              <img src={creatorImage}></img>
+              <p>Creation of <span>Jules Wyvern</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
